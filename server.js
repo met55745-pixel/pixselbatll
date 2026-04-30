83fb936160206f221ee06cf8e72319d3c1eca0df
met55745-pixel <met55745@gmail.com>
2026-04-30 23:56:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,29 +5,31 @@ const io = require('socket.io')(http, { cors: { origin: "*" } });
 const path = require('path');
 
 let mapState = {}; 
+let players = {}; // Храним позиции всех игроков
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
-    // Рассылаем онлайн всем
     io.emit('online_stats', io.engine.clientsCount);
-
-    // Отправляем карту новому игроку
     socket.emit('init_map', mapState);
 
-    // Логика покраски
+    // Принимаем позицию игрока и рассылаем всем
+    socket.on('update_position', (data) => {
+        players[data.uid] = { lat: data.lat, lng: data.lng, color: data.color };
+        io.emit('players_nearby', players);
+    });
+
     socket.on('paint_pixel', (data) => {
-        // data.uid - это зашифрованный ID игрока
         mapState[data.id] = { color: data.color, owner: data.uid };
         io.emit('pixel_updated', data);
     });
 
     socket.on('disconnect', () => {
+        // Удаляем игрока из списка при выходе
+        // (Для простоты оставим, пока не обновится онлайн)
         io.emit('online_stats', io.engine.clientsCount);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log('GeoMetka Server LIVE on port ' + PORT);
-});
+http.listen(PORT, () => { console.log('Server running on ' + PORT); });
